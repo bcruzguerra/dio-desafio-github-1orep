2a9bbb4e39ce20a74943f3d27f918e42a2965f4b
bcruzguerra <bcruzguerra@outlook.com>
2022-01-31 19:18:36 -0300

    Primeiro commit

---
+++ src/dio/com/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package dio.com;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
+		System.out.println("Go fuck yourself. "+(a+b));
+
+	}
+
+}
